8dcf679110852ce6a749bcbedb9d75c5cb5e567e
HaonanWu <whn289467822@outlook.com>
2017-03-23 23:25:28 +0800

    modify browser

--- management/views.py
+++ management/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, JsonResponse
-from django.contrib.auth.decorators import user_passes_test, login_required
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
