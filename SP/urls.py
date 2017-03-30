136c36da44ff1d5ad772cc5e1e6f89b26f1a83c2
HaonanWu <whn289467822@outlook.com>
2017-03-30 14:12:35 +0800

    Update urls.py

--- SP/urls.py
+++ SP/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls, name='admin'),
     url(r'', include('management.urls'))
 ]
 
